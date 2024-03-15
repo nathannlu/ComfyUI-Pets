0aa906d1e3d3adcf2b06479daf9c52188e14f874
dennistruong <truong.dennis00@gmail.com>
2024-03-15 16:43:35 -0700

    fix: removes unecessary repeated object creation

--- js/comfy/comfy.js
+++ js/comfy/comfy.js
@@ -90,6 +90,8 @@ export class ComfyNode extends LiteGraph.LGraphNode {
 
   onMouseDown() {
     const [mouseX, mouseY] = this.getRelativeMouseWithinNode();
+    // Adjust mouse hitbox as necessary
+    const mouse = { x: mouseX, y: mouseY, width: 5, height: 5 };
 
     for (let i = 0; i < this.buttons.length; i++) {
       const button = this.buttons[i];
@@ -104,8 +106,6 @@ export class ComfyNode extends LiteGraph.LGraphNode {
       for (let j = 0; j < gameObjects.length; j++) {
         const gameObject = gameObjects[j];
 
-        // Adjust mouse hitbox as necessary
-        const mouse = { x: mouseX, y: mouseY, width: 5, height: 5 };
         if (gameObject.isTouching(mouse)) {
           gameObject.onClick();
         }
